fccbb76583510cb04663c84774f193052f942006
Sana0864sharma <anushaiitjeee@gmail.com>
2026-05-21 00:04:00 +0530

    Day 1 to Day 4 React daily progress

---
+++ 02-componets/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <h1>Hii!MY name is ANUSHA SHARMA</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, hic.</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ 03-props/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <Card user='Anusha' age='18' img='https://images.unsplash.com/photo-1768036479363-0810baba6613?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUzfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D' desc='I am in my 4th year'/>
+        <Card user='Punam' age='25' img='https://images.unsplash.com/photo-1774646599073-44f22189cc48?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDU0fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D' desc='I am preparing for banking exam'/>
+         <Card user='Anusha' age='18' img='https://images.unsplash.com/photo-1768036479363-0810baba6613?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUzfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D' desc='I am in my 4th year'/>
+          <Card user='Anusha' age='18' img='https://images.unsplash.com/photo-1768036479363-0810baba6613?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUzfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D' desc='I am in my 4th year'/>
+    </div>
+  )
+}
+
+export default App
